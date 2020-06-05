be06ab74452069b6f58c49763b5be25264cc931b
spogoff <samin.pogoff@gmail.com>
2020-06-05 16:46:32 -0400

    popup on markers added

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -35,7 +35,7 @@ d3.json(earthQuakes).then(function(data) {
         return {
             opacity: 1,
             fillOpacity: 1,
-            fillColor: "#FFAE42",
+            fillColor: getColor(feature.properties.mag),
             color: "#000000",
             radius: getRadius(feature.properties.mag),
             stroke: true,
@@ -50,6 +50,26 @@ d3.json(earthQuakes).then(function(data) {
         }
         return magnitude * 4;
     }
+    // This function determines the color of the circle based on the magnitude of the earthquake.
+    function getColor(magnitude){
+        if(magnitude > 5){
+            return "#ea2c2c";
+        }
+        if(magnitude > 4){
+            return "#ea822c";
+        }
+        if(magnitude > 3){
+            return "#ee9c00";
+        }
+        if(magnitude > 2){
+            return "#eecc00";
+        }
+        if(magnitude > 1){
+            return "#d4ee00";
+        }
+        return "#98ee00";
+    }
+
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJson(data, {
         // We turn each feature into a circleMarker on the map.
@@ -58,6 +78,9 @@ d3.json(earthQuakes).then(function(data) {
             return L.circleMarker(latlng);
         },
         // We set the style for each circleMarker using our styleInfo function.
-        style: styleInfo
+        style: styleInfo,
+        onEachFeature: function(feature,layer){
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+        }
     }).addTo(map);
 });
